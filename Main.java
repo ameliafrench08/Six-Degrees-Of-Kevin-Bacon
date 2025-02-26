e5d7735c9dcd50c09e16038eecc31b3c0d9b6db5
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2025-02-26 10:53:09 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<SimpleMovie> movies = MovieDatabaseBuilder.getMovieDB("src/movie_data");
+
+        int numTrue = 0;
+        for (SimpleMovie movie : movies) {
+            System.out.println(movie);
+            if (movie.searchCast("Leonardo DiCaprio")){
+                numTrue++;
+            }
+
+        }
+
+        System.out.println("NUM TRUE: " + numTrue);
+         System.out.println("Number of movies: " + movies.size());
+
+
+    }
+}
---
+++ SimpleMovie.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SimpleMovie {
+    private String title;
+    private String actorsData;
+    private ArrayList<String> actors;
+    private ArrayList<String> kevinBaconMovies = new ArrayList<>();
+
+
+    public SimpleMovie(String t, String a) {
+        title = t;
+        actorsData = a;
+        actors = new ArrayList<String>();
+        String[] tempActors = actorsData.split(":");
+        for (int i = 0; i < tempActors.length; i++) {
+            actors.add(tempActors[i]);
+        }
+
+    }
+
+    public boolean searchCast(String searchTerm) {
+        Scanner scanner = new Scanner(System.in);
+
+        // prevent case sensitivity
+        searchTerm = searchTerm.toLowerCase();
+
+        // arraylist to hold search results
+        ArrayList<String> results = new ArrayList<String>();
+
+        // search through ALL movies in collection
+        for (int i = 0; i < actors.size(); i++) {
+            String actorName = actors.get(i);
+            actorName = actorName.toLowerCase();
+
+            if (actorName.contains(searchTerm)) {
+                //add the Movie objest to the results list
+                results.add(actors.get(i));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<SimpleMovie> kevinBaconMovies(){
+        ArrayList<SimpleMovie> movies = MovieDatabaseBuilder.getMovieDB("src/movie_data");
+        ArrayList<SimpleMovie> kevinBaconMovies = new ArrayList<SimpleMovie>();
+
+        assert movies != null;
+        for (SimpleMovie movie : movies){
+            if (movie.searchCast("Kevin Bacon")){
+                kevinBaconMovies.add(movie);
+            }
+        }
+        return kevinBaconMovies;
+    }
+
+    public String toString() {
+        return "Title: " + title + "\n" + "Actors: " + actors + "\n";
+    }
+}
